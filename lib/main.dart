435b83a700e71346263c0a59f0c7991537ee40e1
m0hamed98ayman <140635923+m0hamed98ayman@users.noreply.github.com>
2024-07-23 23:09:27 +0300

    changing the theme and making notes_views folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_views.dart';
 
 void main() {
-runApp(NotesApp());
-
+  runApp(NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
@@ -10,6 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: NotesView(),
+    );
   }
 }
