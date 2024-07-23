cb5ae28fb0e7ecc5149bb35bb062b7a22dc3dec1
m0hamed98ayman <140635923+m0hamed98ayman@users.noreply.github.com>
2024-07-23 23:11:12 +0300

    adding const keyword

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/notes_views.dart';
 
 void main() {
-  runApp(NotesApp());
+  runApp( const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
@@ -13,7 +13,7 @@ class NotesApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: NotesView(),
+      home: const NotesView(),
     );
   }
 }
