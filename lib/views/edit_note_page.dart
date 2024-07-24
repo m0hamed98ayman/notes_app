1a31a389a4b8ce37624b88e1766ee661a2d702b0
m0hamed98ayman <140635923+m0hamed98ayman@users.noreply.github.com>
2024-07-24 19:56:53 +0300

    making the edit note page

---
+++ lib/views/edit_note_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditNotePage extends StatelessWidget {
+  const EditNotePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor:  Color(0xff303030),
+      
+    );
+  }
+}
--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -1,58 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_page.dart';
 
 class CustomCard extends StatelessWidget {
   const CustomCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 16, top: 16,bottom: 16),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color.fromARGB(255, 250, 217, 69)),
-      child: Column(
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter tips',
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only( top: 16.0),
-              child: Text(
-                'Build Your Career With Mohamed Ayman',
+    return GestureDetector(
+       onTap:(){
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNotePage();
+        },));
+       } ,
+      child: Container(
+        padding: const EdgeInsets.only(left: 16, top: 16,bottom: 16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: const Color.fromARGB(255, 250, 217, 69)),
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(0.55),
-                  fontSize: 18
+                  fontSize: 30,
                 ),
               ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 20.0),
+              subtitle: Padding(
+                padding: const EdgeInsets.only( top: 16.0),
                 child: Text(
-                  'Jul 24 , 2024',
+                  'Build Your Career With Mohamed Ayman',
                   style: TextStyle(
                     color: Colors.black.withOpacity(0.55),
+                    fontSize: 18
                   ),
                 ),
               ),
-            ],
-          )
-        ],
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: Text(
+                    'Jul 24 , 2024',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.55),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
