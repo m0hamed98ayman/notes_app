799558be3eb2120dc61fc7009ae9c86825a22892
m0hamed98ayman <140635923+m0hamed98ayman@users.noreply.github.com>
2024-07-25 20:32:27 +0300

    init the hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/notes_views.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 
@@ -13,7 +17,7 @@ class NotesApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Poppins'),
       debugShowCheckedModeBanner: false,
-      home:const NotesViewPage(),
+      home: const NotesViewPage(),
     );
   }
 }
