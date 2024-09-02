16fa3bbd12702a0648669dcee2159dcbc8e17be2
m0hamed98ayman <140635923+m0hamed98ayman@users.noreply.github.com>
2024-09-03 02:10:05 +0300

    adding states to note_cubit

--- lib/cubits/notes_cubit/note_state.dart
+++ lib/cubits/notes_cubit/note_state.dart
@@ -4,3 +4,12 @@ part of 'note_cubit.dart';
 sealed class NoteState {}
 
 final class NoteInitial extends NoteState {}
+final class NoteLoading extends NoteState {}
+final class NoteSuccess extends NoteState {}
+final class NoteFailed extends NoteState {
+final String errMessage;
+NoteFailed(this.errMessage);
+}
+
+
+
